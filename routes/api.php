dd7ea2b959b23645b283e9bb4b62878c23097419
mondrian2 <deecomunicacao@gmail.com>
2021-02-20 11:44:24 -0300

    insere controller exercicios

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('items', 'Api\ItemsController', ['except' => ['create', 'edit']]); 
+Route::resource('items', 'Api\ItemsController'); 
+Route::resource('exercicios', 'Api\ExerciciosController'); 
