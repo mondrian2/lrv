5b0f3eb0c0816cef2d1c136fc6d3768eeddda7d5
mondrian2 <deecomunicacao@gmail.com>
2021-02-22 17:10:22 -0300

    cors3

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -72,8 +72,18 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
+        // Route::group([
+        //     'middleware' => ['api', 'cors'],
+        //     'namespace' => $this->namespace,
+        //     'prefix' => 'api',
+        // ], function ($router) {
+        //      //Add you routes here, for example:
+        //      Route::apiResource('/posts','PostController');
+        // });
+
         Route::prefix('api')
             ->middleware('api')
+            ->middleware('cors')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
