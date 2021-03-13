11468945f346727e226b861d97c3440165934e3c
daniel <deecomunicacao@gmail.com>
2021-03-13 13:16:49 -0300

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,12 +18,9 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        public function handle($request, Closure $next)
-        {
-            return $next($request)
-                ->header('Access-Control-Allow-Origin', "*")
-                ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
-                ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
-        }
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', "*")
+            ->header('Access-Control-Allow-Methods', "PUT, POST, DELETE, GET, OPTIONS")
+            ->header('Access-Control-Allow-Headers', "Accept, Authorization, Content-Type");
     }
 }
