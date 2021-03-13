30368a9ab0d2af633d999e361d5c8f3c90e4bf8a
daniel <deecomunicacao@gmail.com>
2021-03-13 09:42:37 -0300

    cors

--- routes/api.php
+++ routes/api.php
@@ -24,29 +24,33 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('items', 'Api\ItemsController');
 
-Route::get('/exercises-alternative', [ExerciseAlternativeController::class, 'index']);
-Route::get('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'show']);
-Route::post('/exercise-alternative', [ExerciseAlternativeController::class, 'store']);
-Route::put('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'update']);
-Route::delete('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'destroy']);
+Route::middleware(['cors'])->group(function () {
 
-Route::get('/exercises-complete', [ExerciseCompleteController::class, 'index']);
-Route::get('/exercise-complete/{id}', [ExerciseCompleteController::class, 'show']);
-Route::post('/exercise-complete', [ExerciseCompleteController::class, 'store']);
-Route::put('/exercise-complete/{id}', [ExerciseCompleteController::class, 'update']);
-Route::delete('/exercise-complete/{id}', [ExerciseCompleteController::class, 'destroy']);
+    Route::get('/exercises-alternative', [ExerciseAlternativeController::class, 'index']);
+    Route::get('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'show']);
+    Route::post('/exercise-alternative', [ExerciseAlternativeController::class, 'store']);
+    Route::put('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'update']);
+    Route::delete('/exercise-alternative/{id}', [ExerciseAlternativeController::class, 'destroy']);
 
-Route::get('/exercicios', [ExerciciosController::class, 'index']);
-Route::get('/exercicio/{id}', [ExerciciosController::class, 'show']);
-Route::post('/exercicio', [ExerciciosController::class, 'store']);
-Route::put('/exercicio/{id}', [ExerciciosController::class, 'update']);
-Route::delete('/exercicio/{id}', [ExerciciosController::class, 'destroy']);
+    Route::get('/exercises-complete', [ExerciseCompleteController::class, 'index']);
+    Route::get('/exercise-complete/{id}', [ExerciseCompleteController::class, 'show']);
+    Route::post('/exercise-complete', [ExerciseCompleteController::class, 'store']);
+    Route::put('/exercise-complete/{id}', [ExerciseCompleteController::class, 'update']);
+    Route::delete('/exercise-complete/{id}', [ExerciseCompleteController::class, 'destroy']);
 
-Route::get('/itens', [ItemsController::class, 'index']);
-Route::get('/item/{id}', [ItemsController::class, 'show']);
-Route::post('/item', [ItemsController::class, 'store']);
-Route::put('/item/{id}', [ItemsController::class, 'update']);
-Route::delete('/item/{id}', [ItemsController::class, 'destroy']);
+    Route::get('/exercicios', [ExerciciosController::class, 'index']);
+    Route::get('/exercicio/{id}', [ExerciciosController::class, 'show']);
+    Route::post('/exercicio', [ExerciciosController::class, 'store']);
+    Route::put('/exercicio/{id}', [ExerciciosController::class, 'update']);
+    Route::delete('/exercicio/{id}', [ExerciciosController::class, 'destroy']);
+
+    Route::get('/itens', [ItemsController::class, 'index']);
+    Route::get('/item/{id}', [ItemsController::class, 'show']);
+    Route::post('/item', [ItemsController::class, 'store']);
+    Route::put('/item/{id}', [ItemsController::class, 'update']);
+    Route::delete('/item/{id}', [ItemsController::class, 'destroy']);
+
+}
 
 
 
