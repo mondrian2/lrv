bc1092515906b734f0278485d45450d5b269b6f6
daniel <deecomunicacao@gmail.com>
2021-03-14 08:38:19 -0300

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -53,8 +53,5 @@ $app->singleton(
 |
 */
 
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
 return $app;
