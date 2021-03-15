c81335d7b7b4d32bac8d89cf00c91010ca405ab4
daniel <deecomunicacao@gmail.com>
2021-03-14 22:38:41 -0300

    plural controller

--- app/Http/Controllers/Api/ExercisesController.php
+++ app/Http/Controllers/Api/ExercisesController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Exercise;
 
-class ExerciseController extends Controller
+class ExercisesController extends Controller
 {
     public $model = Exercise::class;
 
